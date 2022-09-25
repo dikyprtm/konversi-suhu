1545a6942dafdff0b367e5fbee94c43a467a5f2a
LIezi <43513141+dikyprtm@users.noreply.github.com>
2022-09-25 22:07:22 +0700

    Create suhu.py

---
+++ suhu.py
@@ -0,0 +1,31 @@
+c = float(input("Input suhu dalam celcius(c) : \n"))
+
+konversi = (input("""Konversi kemana?? 
+- Ketikkan F atau f untuk konversi ke Fahrenheit
+- Ketikkan K atau k untuk konversi ke Kelvin
+- Ketikkan R atau r untuk konversi ke Reamur
+- Ketikkan A atau ALL untuk menampilkan semua konversi
+"""))
+print("=============================================")
+
+
+match konversi:
+    case "F" | "f":
+        fahrenheit = (9/5) * c + 32
+        print("Hasil konversi dari",c,"ke F adalah", fahrenheit)
+
+    case "K" | "k":
+        kelvin = c + 32
+        print("Hasil konversi dari",c,"ke K adalah", kelvin)
+
+    case "R" | "r":
+        reamur = (4/5) * c
+        print("Hasil konversi dari",c,"ke R adalah", reamur)
+
+    case "A" | "ALL":
+        fahrenheit = (9/5) * c + 32
+        kelvin = c + 32
+        reamur = (4/5) * c
+        print("Hasil konversi dari",c,"ke F adalah", fahrenheit)
+        print("Hasil konversi dari",c,"ke K adalah", kelvin)
+        print("Hasil konversi dari",c,"ke R adalah", reamur)
